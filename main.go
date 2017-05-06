0632e2509906304ccc8817655aadabe63e370ebe
David Laban <alsuren@gmail.com>
2017-05-06 16:30:52 +0100

    import and use go-messenger-bot for /webhook subscriptions

--- main.go
+++ main.go
@@ -6,13 +6,26 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/abhinavdahiya/go-messenger-bot"
 )
 
 func main() {
 	port := os.Getenv("PORT")
+    access_token := os.Getenv("ACCESS_TOKEN")
+    app_secret := os.Getenv("APP_SECRET")
+    webhook_verify_token := os.Getenv("WEBHOOK_VERIFY_TOKEN")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
+	}
+	if access_token == "" {
+		log.Fatal("$ACCESS_TOKEN must be set")
+	}
+	if app_secret == "" {
+		log.Fatal("$APP_SECRET must be set")
+	}
+	if webhook_verify_token == "" {
+		log.Fatal("$WEBHOOK_VERIFY_TOKEN must be set")
 	}
 
 	router := gin.New()
@@ -24,5 +37,18 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
-	router.Run(":" + port)
+	bot := mbotapi.NewBotAPI(access_token, webhook_verify_token, app_secret)
+	callbacks, mux := bot.SetWebhook("/webhook")
+
+	router.GET("/webhook", gin.WrapH(mux))
+	router.POST("/webhook", gin.WrapH(mux))
+
+	go router.Run(":" + port)
+
+	for callback := range callbacks {
+        log.Printf("[%#v] %s", callback.Sender, callback.Message.Text)
+
+        msg := mbotapi.NewMessage(callback.Message.Text)
+        bot.Send(callback.Sender, msg, mbotapi.RegularNotif)
+    }
 }
