fa3cda0ec01abc4fe3599aff4c212e17028a5db6
David Laban <alsuren@gmail.com>
2017-05-07 00:41:30 +0100

    add user's full name to the messages

--- main.go
+++ main.go
@@ -14,7 +14,8 @@ import (
 )
 
 var (
-	db *sql.DB
+	db  *sql.DB
+	bot *mbotapi.BotAPI
 )
 
 func dbFunc(c *gin.Context) {
@@ -25,11 +26,18 @@ func dbFunc(c *gin.Context) {
 		if c.Query("delete") == "true" {
 			deleteUser(id)
 		} else {
-			insertUser(id)
+			fullUser, err := insertUser(id)
+			if err != nil {
+				fmt.Printf("Error reading full user: %q", err)
+				return
+			}
+			c.String(http.StatusOK, fmt.Sprintf("Full User: %s %s\n", fullUser.FirstName, fullUser.LastName))
 		}
 	}
 
-	rows, err := db.Query("SELECT id FROM users WHERE last_seen > now() - '1 hour'::INTERVAL")
+	rows, err := db.Query(`
+		SELECT id FROM users
+		WHERE last_seen > now() - '1 hour'::INTERVAL`)
 	if err != nil {
 		c.String(http.StatusInternalServerError,
 			fmt.Sprintf("Error reading ids: %q", err))
@@ -52,17 +60,83 @@ func setupUsers() {
 	// if _, err := db.Exec("DROP TABLE users"); err != nil {
 	//     log.Printf("Error dropping database table: %q", err)
 	// }
-	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS users (last_seen timestamp, id bigint, CONSTRAINT users_uq unique (id))"); err != nil {
+	if _, err := db.Exec(`
+			CREATE TABLE IF NOT EXISTS users (
+				last_seen timestamp, id bigint,
+				CONSTRAINT users_uq UNIQUE (id)
+			)`); err != nil {
+		log.Printf("Error creating database table: %q", err)
+	}
+	if _, err := db.Exec(`
+			CREATE TABLE IF NOT EXISTS full_users (
+				id bigint,
+				first_name text,
+				last_name text,
+				profile_pic text,
+				locale text,
+				timezone bigint,
+				gender text,
+				CONSTRAINT full_users_uq UNIQUE (id)
+			)`); err != nil {
 		log.Printf("Error creating database table: %q", err)
 	}
 }
 
-func insertUser(id int64) {
-	if _, err := db.Exec("INSERT INTO users (last_seen, id) VALUES (now(), $1) ON CONFLICT (id) DO UPDATE SET last_seen=now()", id); err != nil {
+func insertUser(id int64) (mbotapi.FullUser, error) {
+	fullUser := mbotapi.FullUser{User: mbotapi.NewUserFromID(id)}
+
+	if _, err := db.Exec(`
+			INSERT INTO users (last_seen, id)
+			VALUES (now(), $1)
+			ON CONFLICT (id) DO UPDATE SET last_seen=now()
+			`, id); err != nil {
 		log.Printf("Error adding user: %q", err)
-		return
+		return fullUser, err
+	}
+
+	err := db.QueryRow(`
+		SELECT (first_name, last_name, profile_pic, locale, timezone, gender)
+		from full_users WHERE id=$1`, id).Scan(
+		&fullUser.FirstName,
+		&fullUser.LastName,
+		&fullUser.ProfilePic,
+		&fullUser.Locale,
+		&fullUser.Timezone,
+		&fullUser.Gender)
+
+	if err == nil {
+		return fullUser, nil
+	} else if err != nil && err != sql.ErrNoRows {
+		log.Fatalf("error checking if row exists %v", err)
+		return fullUser, err
+	} else /* if err == sql.ErrNoRows */ {
+		fullUser, err := bot.GetFullUser(mbotapi.NewUserFromID(id))
+		if err != nil {
+			log.Fatalf("error fetching full user %v", err)
+			return fullUser, err
+		}
+
+		_, err = db.Exec(`
+				INSERT INTO full_users (id, first_name, last_name, profile_pic, locale, timezone, gender)
+				VALUES ($1, $2, $3, $4, $5, $6, $7)
+				ON CONFLICT (id) DO NOTHING`,
+			fullUser.User.ID,
+			fullUser.FirstName,
+			fullUser.LastName,
+			fullUser.ProfilePic,
+			fullUser.Locale,
+			fullUser.Timezone,
+			fullUser.Gender)
+
+		if err != nil {
+			log.Fatalf("error inserting full user %v", err)
+			return fullUser, err
+		} else {
+			return fullUser, nil
+		}
 	}
 }
+
 func deleteUser(id int64) {
 	if _, err := db.Exec("DELETE FROM users WHERE id=$1", id); err != nil {
 		log.Printf("Error deleting user: %q", err)
@@ -77,15 +151,23 @@ func forwardToUsers(bot *mbotapi.BotAPI, callback mbotapi.Callback) {
 		return
 	}
 
-	insertUser(callback.Sender.ID)
-	rows, err := db.Query("SELECT id FROM users WHERE last_seen > now() - '1 hour'::INTERVAL")
+	fullUser, err := insertUser(callback.Sender.ID)
+	if err != nil {
+		log.Printf("Error getting full user: %q", err)
+		return
+	}
+	rows, err := db.Query(`
+		SELECT id FROM users
+		WHERE last_seen > now() - '1 hour'::INTERVAL`)
 	if err != nil {
 		log.Printf("Error reading users: %q", err)
 		return
 	}
 	defer rows.Close()
 
-	msg := mbotapi.NewMessage(callback.Message.Text)
+	msg := mbotapi.NewMessage(
+		fmt.Sprintf(
+			"%s %s: %s", fullUser.FirstName, fullUser.LastName, callback.Message.Text))
 	for rows.Next() {
 		var id int64
 		if err := rows.Scan(&id); err != nil {
@@ -130,7 +212,7 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
-	bot := mbotapi.NewBotAPI(access_token, webhook_verify_token, app_secret)
+	bot = mbotapi.NewBotAPI(access_token, webhook_verify_token, app_secret)
 	callbacks, mux := bot.SetWebhook("/webhook")
 	bot.Debug = true
 
